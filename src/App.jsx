82447a6db5b8c48e4a00cde868da511a1c645bbf
dmytromayor <mayor4090@gmail.com>
2023-03-20 23:35:19 +0200

    Add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import React from "react";
 
 export default function App() {
-  return (
-    <div></div>
-  )
+  return <div></div>;
 }
