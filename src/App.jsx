10f5d640aa7b6eedfaa0bfb512a5c2778db8589a
dmytromayor <mayor4090@gmail.com>
2023-03-20 23:45:31 +0200

    Create basic components and layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import React from "react";
 
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
